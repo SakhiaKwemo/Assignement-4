0194f417aaa7dc7f5746db85a5e6e1af423c25bd
SakhiaKwemo <45702852+SakhiaKwemo@users.noreply.github.com>
2020-01-15 17:54:18 -0500

    Create Bolt.java

---
+++ Bolt.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+
+public class Bolt extends OuterThread implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6729458439608083110L;
+
+	public Bolt(double l, String d, String m, String f, double u, double n) throws IllegalFastener{
+		
+		super(l, d, m, f, u, n); 
+	}
+	
+	public String toString() {
+		
+		return super.toString(); 
+	}
+}
