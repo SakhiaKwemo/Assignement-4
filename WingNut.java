521485da8b47ba25c77c94fd53e07594b1367a06
SakhiaKwemo <45702852+SakhiaKwemo@users.noreply.github.com>
2020-01-15 17:51:05 -0500

    Create WingNut.java

---
+++ WingNut.java
@@ -0,0 +1,20 @@
+import java.io.Serializable;
+
+public class WingNut extends InnerThread implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7019926791953693666L;
+
+	public WingNut(String d, String m, String f, double u, double n) throws IllegalFastener{
+		
+		super(d, m, f, u, n); 
+	}
+	
+	public String toString() {
+		
+		return "Wing Nut " + super.toString(); 
+	}
+
+}
